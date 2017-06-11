fa60bc856d967736f8033df8291f82676147a906
prabhakaran18 <prabhakaranmgv@gmail.com>
2017-06-11 14:19:43 +0530

    Create pro-one.c

---
+++ pro-one.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d",&n);
+	if(n>0)
+	  printf("positive");
+	else if(n==0)
+	  printf("zero");
+	else
+	  printf("negative");
+	// your code goes here
+	return 0;
+}
